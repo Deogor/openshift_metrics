3bb8ece902323f16c435bad9ee4c5c61cab50ad6
Deogor <74254811+Deogor@users.noreply.github.com>
2022-03-14 18:32:42 +0300

    Create metrics.py

---
+++ metrics.py
@@ -0,0 +1,31 @@
+import os
+import time
+import socket
+import shutil
+
+while True:
+  hostname = os.uname()[1]
+  total, used, free = shutil.disk_usage("/")
+  f = open('/data/index.html', 'w')
+  f.write('network info' + '\n')
+  f.write(hostname + '\n')
+  f.write(socket.gethostbyname(socket.gethostname()) + '\n')
+  f.write(socket.gethostbyname(socket.getfqdn()) + '\n')
+  f.write('Disk Info' + '\n')
+  f.write("Total: %d GiB" % (total // (2**30)) + '\n')
+  f.write("Used: %d GiB" % (used // (2**30)) + '\n')
+  f.write("Free: %d GiB" % (free // (2**30)) + '\n')
+
+  print('I run!')
+  print(hostname)
+
+  print(socket.gethostbyname(socket.gethostname()))
+  print(socket.gethostbyname(socket.getfqdn()))
+
+  print("Total: %d GiB" % (total // (2**30)))
+  print("Used: %d GiB" % (used // (2**30)))
+  print("Free: %d GiB" % (free // (2**30)))
+
+  time.sleep(10)
+#  break
+#print('Not in loop!')
